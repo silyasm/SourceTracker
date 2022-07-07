9594f2fc458d4cd79923d8621bb69a5325810c23
Ilyas SULI22 <ilyas@clswl069.cels.anl.gov>
2022-07-07 13:18:30 -0500

    added my python algorithm to Impl.py file

--- lib/SourceTracker/SourceTrackerImpl.py
+++ lib/SourceTracker/SourceTrackerImpl.py
@@ -6,6 +6,15 @@ import uuid
 from SourceTracker.TAUtils import run
 from installed_clients.KBaseReportClient import KBaseReport
 from installed_clients.DataFileUtilClient import DataFileUtil
+from importlib.metadata import metadata
+from itertools import accumulate
+import random
+from random import randrange, sample
+from re import A
+import pandas
+import numpy as np
+from IPython.display import display
+import csv
 
 #END_HEADER
 
@@ -63,7 +72,12 @@ class SourceTracker:
         # ctx is the context object
         # return variables are: output
         #BEGIN run_SourceTracker
-
+        for name in ['sink_label', 'source_label']:
+            if name not in params['source_type']:
+                raise ValueError('Parameter "' + name + '" is required but missing')
+        #for type in params['sample_type']:
+        #   if not isinstance(type == params['sink_label'] and type == params['source_label']
+        #       raise ValueError('Label "' + type + '" does not match to sink or source label')
         logging.info('start run_SourceTracker with:\n{}'.format(params))
 
         # extract params
@@ -83,6 +97,57 @@ class SourceTracker:
                                   dfu=self.dfu, scratch=self.shared_folder,
                                   source_label=source_label,
                                   ascending=ascending)
+        for sample in params['sample_type']:
+            if sample. == params['sink_label']:
+                sink_otus.append('amplicon_matrix_ref'.loc['sample'])
+        
+        otuRowsStrings = []
+        with open('amplicon_matrix_ref', 'r') as file:
+            csvreader = csv.reader(file)
+            sources = next(csvreader)
+            for row in csvreader:
+                otuRowsStrings.append(row)
+        OTUs = []
+        abundanceData = []
+        for row in otuRowsStrings:
+            OTUs.append(row.pop(0))
+            row = [int(i) for i in row]
+            abundanceData.append(row)
+        sources.remove('')
+
+        table_otu = pandas.DataFrame(data = abundanceData, columns=[sources], index=[OTUs])
+        #display(table_otu)
+
+        table_p_tv = table_otu.loc[table_otu.index].div(table_otu.sum(axis=0), axis=1)
+        #display(table_p_tv)
+
+        sink_otu_sources = []
+        for x in range(len(OTUs)):
+            sink_otu_sources.append(randrange(1, len(OTUs)))
+
+        table_otu_reference = pandas.DataFrame(data=[sink_otu_sources], columns=[OTUs], index= ['Random Sources'])
+        #display(table_otu_reference)
+
+        def roll(probs):
+            cumulativeWeights = list(accumulate(probs))
+            position = random.random()
+            for side, endPosition in enumerate(cumulativeWeights):
+                if position < endPosition:
+                    return side + 1
+
+        predictedSources = []
+        p_tv = []
+
+        for i in OTUs:
+            p_v = np.bincount(sink_otu_sources, minlength=3)[1:] / float(len(sink_otu_sources))
+            p_tv = np.array([(table_p_tv.at[i, sources[0]])])
+            comboProbability = p_tv * p_v
+            prob = comboProbability / comboProbability.sum()
+            predictedSource = roll(prob)
+            sink_otu_sources[i] = predictedSource
+            predictedSources.append(predictedSource)
+        table_prediction = pandas.DataFrame(data=[sink_otus, sink_otu_sources], index=['OTU', 'Predicted Source'])
+        return(table_prediction)
 
         report_client = KBaseReport(self.callback_url, token=self.token)
         report_name = "SourceTracker_report_" + str(uuid.uuid4())
